95d5484b01f1eeba26d2d088fa53437564fa5d31
Jefry0727 <jjmb2789@gmail.com>
2017-04-13 21:04:43 -0500

    listar

--- src/main/java/co/edu/eam/controller/CarritoController.java
+++ src/main/java/co/edu/eam/controller/CarritoController.java
@@ -3,6 +3,8 @@
  */
 package co.edu.eam.controller;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -14,48 +16,42 @@ import co.edu.eam.model.Carrito;
 import co.edu.eam.model.Producto;
 
 /**
- * @author Jefry Londoño <jjmb2789@gmail.com>
- * @13/04/2017
- * @version 
+ * @author Jefry Londoño <jjmb2789@gmail.com> @13/04/2017
+ * @version
  */
 
 @WebService(name = "CarritoController", portName = "CarriControPort", targetNamespace = "http://co.edu.eam.ingsoft.distribuidos")
 public class CarritoController {
-	
+
 	@EJB
 	private PersistenceManagerLocal persistencia;
-	
+
 	@WebMethod(action = "AgregarCarrito", operationName = "operacionAgregarCarrito")
-	public boolean agregarCarrito(@WebParam(name= "carrito")Carrito carrito){
-		
-		if(validarProductos(carrito)){
-			
-			try{
-				
+	public boolean agregarCarrito(@WebParam(name = "carrito") Carrito carrito) {
+
+		if (validarProductos(carrito)) {
+
+			try {
+
 				persistencia.persist(carrito);
-				
+
 				return true;
-				
-			}catch (Exception e) {
+
+			} catch (Exception e) {
 
 				return false;
-				
+
 			}
-			
-		}else{
-			
+
+		} else {
+
 			return false;
 		}
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	public boolean validarProductos(Carrito carrito){
-		
+
+	public boolean validarProductos(Carrito carrito) {
+
 		Query querie = persistencia.createQuery("SELECT p FROM Producto f where p.id='" + carrito.getId() + "'");
 
 		Producto produc = (Producto) querie.getSingleResult();
@@ -65,9 +61,20 @@ public class CarritoController {
 			return false;
 
 		}
-		
+
 		return true;
-		
+
+	}
+
+	@WebMethod
+	public List<Carrito> listarCarrito() {
+
+		Query query = persistencia.createQuery("SELECT p FROM Carrito p ");
+
+		List<Carrito> lista = (List<Carrito>) query.getResultList();
+
+		return lista;
+
 	}
 
 }
