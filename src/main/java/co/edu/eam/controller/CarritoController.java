b1ccc7b16a5a61ae6e6d81e4ce2207f4e23f7366
Jefry0727 <jjmb2789@gmail.com>
2017-04-13 20:44:37 -0500

    commmit

--- src/main/java/co/edu/eam/controller/CarritoController.java
+++ src/main/java/co/edu/eam/controller/CarritoController.java
@@ -7,9 +7,11 @@ import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.persistence.Query;
 
 import co.edu.eam.ejb.PersistenceManagerLocal;
 import co.edu.eam.model.Carrito;
+import co.edu.eam.model.Producto;
 
 /**
  * @author Jefry Londoño <jjmb2789@gmail.com>
@@ -26,19 +28,44 @@ public class CarritoController {
 	@WebMethod
 	public boolean agregarCarrito(@WebParam(name= "carrito")Carrito carrito){
 		
-		try{
+		if(validarProductos(carrito)){
 			
-			persistencia.persist(carrito);
-			
-			return true;
-			
-		}catch (Exception e) {
+			try{
+				
+				persistencia.persist(carrito);
+				
+				return true;
+				
+			}catch (Exception e) {
 
-			return false;
+				return false;
+				
+			}
 			
+		}else{
+			
+			return false;
 		}
 		
 		
+		
+		
+		
+	}
+	
+	public boolean validarProductos(Carrito carrito){
+		
+		Query querie = persistencia.createQuery("SELECT p FROM Producto f where p.id='" + carrito.getId() + "'");
+
+		Producto produc = (Producto) querie.getSingleResult();
+
+		if ((produc.getCantidad().intValue()) > (carrito.getCantidad().intValue())) {
+
+			return false;
+
+		}
+		
+		return true;
 		
 	}
 
